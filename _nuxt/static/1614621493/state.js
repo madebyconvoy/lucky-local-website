caf9a15137a483b7b6d6cc4ac03b8d41cf99f43f
FelixSchultze <FelixSchultze@users.noreply.github.com>
2021-03-01 17:58:17 +0000

    deploy: c6aa41ea7b9d1433446595be64b0accb1a76ce7f

---
+++ _nuxt/static/1614621493/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Flucky-local-website\u002F_nuxt\u002Fstatic\u002F1614621493",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002Flucky-local-website\u002F",assetsPath:"\u002Flucky-local-website\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"514bd289"}}}}(null));
