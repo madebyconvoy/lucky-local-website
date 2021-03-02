26d65794ebb65d1608d1361b432c1e4e30765d14
FelixSchultze <FelixSchultze@users.noreply.github.com>
2021-03-02 14:14:44 +0000

    deploy: 2f40b82c00cf32a3b165d82b3578ce3223e5a753

--- _nuxt/static/1614690902/impressum/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Flucky-local-website\u002F_nuxt\u002Fstatic\u002F1614690902",layout:"default",error:a,serverRendered:true,routePath:"\u002Fimpressum",config:{app:{basePath:"\u002Flucky-local-website\u002F",assetsPath:"\u002Flucky-local-website\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"82a5bf5a"}}}}(null));
