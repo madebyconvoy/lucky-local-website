8a048b92a51eaa933bc87ce804dcd273911cdb64
tobiasthaden <tobiasthaden@users.noreply.github.com>
2021-03-03 10:56:59 +0000

    deploy: 9970db57068f5b160be84a951e7f22688d4b7471

--- _nuxt/static/1614765502/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1614765502",layout:"default",error:a,serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a2407352"}}}}(null,"\u002F"));
