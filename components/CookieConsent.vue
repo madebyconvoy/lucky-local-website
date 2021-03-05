b68efb2c43bfae4a3457af3ae9b12f19423712c2
Felix Schultze <fs@convoyinteractive.com>
2021-03-05 14:31:23 +0100

    modified cookie consent styling

--- components/CookieConsent.vue
+++ components/CookieConsent.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="fixed bottom-0 right-0 p-10 bg-gray-100 shadow-lg max-w-xl" v-if="show">
+    <div class="fixed bottom-0 right-0 p-10 bg-blue text-white shadow-xl max-w-xl" v-if="show">
         <p>
-            Cookies werden zur Benutzerführung und Webanalyse verwendet und helfen dabei, diese Website besser zu machen. Wenn du diese Seite weiter benutzt, gehen wir davon aus, dass du mit unseren <nuxt-link class="text-blue underline" to="/datenschutz">Datenschutzbestimmungen</nuxt-link> einverstanden bist.
+            Cookies werden zur Benutzerführung und Webanalyse verwendet und helfen dabei, diese Website besser zu machen. Wenn du diese Seite weiter benutzt, gehen wir davon aus, dass du mit unseren <nuxt-link class="text-white underline" to="/datenschutz">Datenschutzbestimmungen</nuxt-link> einverstanden bist.
         </p>
-        <div class="flex space-x-5 mt-10">
-            <button class="text-blue" type="secondary" @click="cancel">Ablehnen</button>
+        <div class="flex space-x-5 mt-10 justify-between">
+            <button class="text-white font-title font-extrabold" type="secondary" @click="cancel">Ablehnen</button>
             <button @click="consent"><Button class="pointer-events-none">Akzeptieren</Button></button>
         </div>
     </div>
